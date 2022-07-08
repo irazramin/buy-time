e0def208dc507ff7c36b090a003bf03acbb5e435
irazramin <irazramin@gmail.com>
2022-07-09 00:11:04 +0600

    categories page design half complete

--- src/Components/Pages/Categories/Categories.js
+++ src/Components/Pages/Categories/Categories.js
@@ -2,7 +2,124 @@ import React from 'react'
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div className='2xl:w-[65%] md:w-[85%] sm:w-[90%] mx-auto'>
+      <div className='grid grid-cols-12 gap-3'>
+        <div className='col-span-3'>
+          <div className='px-5 py-2 bg-red-50 '>
+            <h2 className='text-lg font-semibold '>Categories</h2>
+            <hr className='mt-3 border-2 border-gray-300'/>
+            <div className='mt-10'>
+              <h2 className='text-2xl font-bold text-red-500'>Brands</h2>
+              <div className='mt-2'>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                      Titan
+                    </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                      Casio
+                    </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                      Cellox
+                    </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                      Fiyta
+                    </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                      Seiko
+                    </label>
+                </div>
+              </div>
+            </div>
+
+            <div className='mt-10'>
+              <h2 className='text-2xl font-bold text-red-500'>Gender</h2>
+              <div className='mt-2'>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                  <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                    Men
+                  </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                  <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                    Women
+                  </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                  <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                    Kids
+                  </label>
+                </div>
+              </div>
+
+
+
+
+            </div>
+
+            <div className='mt-10'>
+              <h2 className='text-2xl font-bold text-red-500'>Materials</h2>
+              <div className='mt-2'>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                  <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                    Leather
+                  </label>
+                </div>
+                <div className="form-check my-1">
+                  <input
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      type="checkbox" value="" id="flexCheckDefault" />
+                  <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
+                    Chain
+                  </label>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          </div>
+        <div className='col-span-9 '>
+          <div className='bg-blue-100'>
+            jhkhiuhi
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
